24cd748b382c1080299b0fdb613ccf568686ad7d
Timeaux <timo.grossman@mac.com>
2026-01-19 18:41:39 -0500

    github update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Compliance-Exercise/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
@@ -17,3 +18,4 @@ export default defineConfig({
     },
   },
 })
+
